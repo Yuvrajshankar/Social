a8d2cb4e63cc9643d4baf2eb98ba1006b50c637a
Yuvrajshankar <yuvrajpandar2004@gmail.com>
2024-03-04 15:14:20 +0530

    little error.

--- server/db/connection.js
+++ server/db/connection.js
@@ -5,7 +5,7 @@ const connectDatabase = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log('MONGO-DB CONNECTED');
     } catch (error) {
-        console.log(`Error: ${err.message}`);
+        console.log(`Error: ${error.message}`);
         process.exit(1);
     }
 };
